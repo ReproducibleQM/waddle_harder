790907ed605ab7556608b3ce599375528d1df5c9
aminerov <aminero1@kent.edu>
2018-02-28 10:30:01 -0500

    update

--- penguinR.R
+++ penguinR.R
@@ -30,10 +30,9 @@ penguin.north=penguin.data[penguin.data$Zone=="Other",]
 penguin.south=penguin.data[penguin.data$Zone=="Gough",]
 
 #subset by location
-
+#Nightingale and Gough grouped together
 penguin.Nightingale=penguin.data[penguin.data$Location=="Nightingale",]
 penguin.Tristan=penguin.data[penguin.data$Location=="Tristan",]
-#Gough and Alex
 penguin.Gough=penguin.data[penguin.data$Location2=="Gough",]
 penguin.Inaccessible=penguin.data[penguin.data$Location=="Inaccessible",]
 
@@ -50,6 +49,10 @@ penguin.n1<-read.csv("C:/Users/Anthony Minerovic/Google Drive/Blackwood Lab/2018
 penguin.new=penguin.data[penguin.data$Year>2001,]
 View(penguin.new)
 
+penguin.Gough.new=penguin.new[penguin.new$Location=="Gough",]
+penguin.Nightingale.new=penguin.new[penguin.new$Location2=="Nightingale",]
+penguin.Tristan.new=penguin.new[penguin.new$Location=="Tristan",]
+penguin.Inaccessible.new=penguin.new[penguin.new$Location=="Inaccessible",]
 
 
 #linear models
@@ -61,11 +64,54 @@ anova(penguin.vol.location)
 
 
 
-penguin2all=lm(Volume~Location*Year*Zone, data=penguin.data)
-anova(penguin2all)
+penguins2all=lm(Volume~Year*Zone, data=penguin.data)
+anova(penguins2all)
 
 
+penguin2all = aov(Volume~Location, data=penguin.data)
+
+TukeyHSD(penguin2all, "Location", ordered=FALSE)
+
+#looks like there's a big difference between gough and the other islands, but the other islands aren't that much different from each other
+
+#now do it for just a-eggs and b-eggs
+
+penguin2a = aov(Volume~Location , data=penguin.a)
+
+TukeyHSD(penguin2a, "Location", ordered=FALSE)
 
 
+penguin2b = aov(Volume~Location , data=penguin.b)
+
+TukeyHSD(penguin2b, "Location", ordered=FALSE)
 
 
+#looking for difference over years on each island separately for all years
+
+Goughlm=lm(Volume~Year, data=penguin.Gough)
+anova(Goughlm)
+
+Tristanlm=lm(Volume~Year, data=penguin.Tristan)
+anova(Tristanlm)
+
+Nightingalelm=lm(Volume~Year, data=penguin.Nightingale)
+anova(Nightingalelm)
+#   nightingale island shows significant change over time for all years
+
+Inaccessiblelm=lm(Volume~Year, data=penguin.Inaccessible)
+anova(Inaccessiblelm)
+
+#looking for difference over years on each island separately from 2002 to 2015
+
+#only worked for gough, which is significant
+Goughnewlm=lm(Volume~Year, data=penguin.Gough.new)
+anova(Goughnewlm)
+
+Tristannewlm=lm(Volume~Year, data=penguin.Tristan.new)
+anova(Tristannewlm)
+
+Nightingalenewlm=lm(Volume~Year, data=penguin.Nightingale.new)
+anova(Nightingalenewlm)
+
+Inaccessiblenewlm=lm(Volume~Year, data=penguin.Inaccessible.new)
+anova(Inaccessiblenewlm)
