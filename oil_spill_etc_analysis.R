dd807acecc094a3eb047cb3ddbe5469e2dcd7a36
Christie Bahlai <cbahlai@kent.edu>
2018-05-29 17:04:13 -0400

    add harvest analysis, tighten up oil spill analysis

--- oil_spill_etc_analysis.R
+++ oil_spill_etc_analysis.R
@@ -23,7 +23,7 @@ penguin1$logvolume<-log(penguin1$Volume)
 penguin1$sqrtvolume<-sqrt(penguin1$Volume)
 
 #now create a model to describe egg volume
-oil.model<-lm(Volume~Zone*oilspill+Decision*oilspill+Year, data=penguin1)
+oil.model<-lm(Volume~Zone*oilspill*Decision, data=penguin1)
 
 summary(oil.model)
 AIC(oil.model)
@@ -52,7 +52,28 @@ summary.penguin.oilspill<-ddply(penguin1, c("Zone", "Decision", "oilspill"), sum
 # every year
 
 # NOTE there is no data between 1999 and 2013 so we can't get all of this, but let's classify the data as we would if 
-# we had these data, anyway
+# we had these data, anyway- that way if more data somehow appeared, we could drop it into the analysis with
+#no need to change anything
 
 penguin1$legislate<-as.factor(ifelse(penguin1$Year>=2011, "Ban", 
                                      ifelse(penguin1$Year>=2006, "Aonly", "NoBan")))
+
+
+#and because we want to look at a year effect, and we want to see if there's anything going on
+#relative to the ban, but there's not a ton of data so we'll just need to look at years directly and ANOVA them
+
+
+#we're interested in linear change with time in these two periods, and by island, but otherwise the analysis is 
+#very similar to the above
+harvest.model<-lm(Volume~Location2*as.factor(Year)*Decision, data=penguin1)
+
+summary(harvest.model)
+AIC(harvest.model)
+anova(harvest.model)
+TukeyHSD(aov(harvest.model))
+#data are very non normal
+shapiro.test(resid(harvest.model))
+
+
+summary.penguin.harvest<-ddply(penguin1, c("Location2", "Decision", "Year"), summarise,
+                                mean.Volume=mean(Volume), N=length(Volume), SE=sd(Volume)/sqrt(N))
