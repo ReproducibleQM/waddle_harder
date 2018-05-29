8451c1ae770d2690a2788541faf24200a3327f4c
radams32 <radams32@kent.edu>
2018-05-29 10:46:35 -0400

    Basic plot commands, changed some plot features

--- sst_plots.R
+++ sst_plots.R
@@ -1,1 +1,10 @@
 # SST plots
+
+plot(SST$index,SST$Tristan,type = 'l',col = 'red')
+lines(SST$index,SST$Gough, col = 'blue')
+tristanlm = lm(SST$Tristan~SST$index)
+goughlm = lm(SST$Gough~SST$index)
+
+#add trendlines for both
+abline(tristanlm,col = 'black',lwd = '3')
+abline(goughlm,col='black',lwd='3')
