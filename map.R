7415d63a3004d8c3f8a6c67cb49d7cd8a739c3ba
radams32 <radams32@kent.edu>
2018-06-19 11:59:20 -0400

    Started to add mean sst contours

--- map.R
+++ map.R
@@ -13,12 +13,24 @@ shapefile_df <- fortify(shapefile)
 worldmap <- data.frame(map("world", plot = FALSE)[c("x", "y")])
 
 smallerworld <- worldmap[worldmap$x < 30 & worldmap$x > -80 & worldmap$y < 30 & worldmap$y > -60,]
-TristanGoughSmall <- worldmap[worldmap$x < -8.5 & worldmap$x > -14.5 & worldmap$y < -35.5 & worldmap$y > -38, ]
+TristanGoughSmall <- worldmap[worldmap$x < -4 & worldmap$x > -18 & worldmap$y < -32 & worldmap$y > -48, ]
 
-#smallerworld <- na.omit(smallerworld)
-#TristanGoughSmall <- na.omit(TristanGoughSmall)
-
+#make little box in the inset map
 insetrect <- data.frame(xmin = -14.5, xmax = -8.5, ymin = -41.5, ymax = -35.5)
+
+#Read in sst csv so that we can find the domain average
+sstdat <- read.csv(file="SST_Data_All.csv", header=F, sep=",")
+sstlatlong <- read.csv(file="SST_Lat_Longs.csv", header=F, sep=",")
+
+#Calculate the gridpoint averages, then reshape the data so that the contours can be plotted easily
+avgs = colMeans(sstdat, na.rm = T, dims = 1)
+meansst_grid = matrix(avgs,nrow=8,byrow = T)
+meansst_grid_smaller = meansst_grid[3:5,3:6]
+newlat = as.data.frame(seq(from = -40, to = -36, by = 2))
+newlong = as.data.frame(seq(from = -14, to = -8, by = 2))
+dat = as.data.frame(meansst_grid_smaller)
+
+
 
 ptheme <- theme(panel.border = element_rect(colour = 'black', size = 1, linetype = 1),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_rect(fill = 'white'),legend.key = element_blank())
 
@@ -31,24 +43,23 @@ a <- ggplot(smallerworld) +
 
 b <- ggplot(TristanGoughSmall) +
   theme_bw(base_size = 22) +
-  geom_path(data = TristanGoughSmall, aes(x, y), colour = "green", fill = "white") +
-  geom_polygon(data = shapefile_df, aes(x = long, y = lat, group = group),colour = 'black', fill = 'white', size = 1)+
+  #geom_polygon(data = TristanGoughSmall, aes(x, y), colour = "green", fill = "white") +
+  geom_polygon(data = shapefile_df, aes(x = long, y = lat, group = group),colour = 'black', fill = 'black', size = 1)+
   annotate("text", x = -12, y = -37.1, label = "Tristan")+
   annotate("text", x = -9.8, y = -40.1, label = "Gough")+
-  annotate("text", x = -13, y = -37.2, label = "Inaccessbile")+
-  annotate("text", x = -12.25, y = -37.5, label = "Nightengale")+
+  annotate("text", x = -13, y = -37.2, label = "Inaccessible")+
+  annotate("text", x = -12.25, y = -37.5, label = "Nightingale")+
   labs(x = 'lon', y = 'lat')+
   xlim(-13.5,-9)
-
-#Read in sst csv so that we can find the domain average
-
-sstdat <- read.csv(file="c:/TheDataIWantToReadIn.csv", header=TRUE, sep=",")
+  geom_contour(mapping = aes(newlong,newlat), data = dat, stat = "contour",
+             position = "identity", ..., lineend = "butt", linejoin = "round",
+             linemitre = 10, na.rm = FALSE, show.legend = T, inherit.aes = TRUE)
   
 grid.newpage()
 
 vpb_ <- viewport(width = 1, height = 1, x = 0.5, y = 0.5)  # the larger map
 vpa_ <- viewport(width = 0.4, height = 0.4, x = 0.8, y = 0.8)  # the inset in upper right
-vpc_ <- viewport(width = 1, height = 1, x = 0.5, y = 0.5) # adding the sst data
+#vpc_ <- viewport(width = 1, height = 1, x = 0.5, y = 0.5) # adding the sst data
 print(b, vp = vpb_)
 print(a, vp = vpa_)
-print(a, vp = vpc_)
+#print(c, vp = vpc_)
--- sst_plots.R
+++ sst_plots.R
@@ -11,17 +11,22 @@ cma_gough = rollmean(SST$Gough,12,na.pad=TRUE)
 #ggplot for timeseries
 
 
-# Base plot with date axis
-p <- ggplot(data = cma_tristan) + 
-  geom_line(color = "#00AFBB", size = 1)
-p
-# Set axis limits c(min, max)
-min <- as.Date("2002-1-1")
-max <- NA
-p + scale_x_date(limits = c(min, max))
+ggplot(data=SST, aes(x=dose, y=len)) +
+  geom_line(linetype = "dashed")+
+  geom_point()
 
 
-#plot away!
+# # Base plot with date axis
+# p <- ggplot(data = cma_tristan) +
+#   geom_line(color = "#00AFBB", size = 1)
+# p
+# # Set axis limits c(min, max)
+# min <- as.Date("2002-1-1")
+# max <- NA
+# p + scale_x_date(limits = c(min, max))
+
+
+# #plot away!
 # xaxes = SST$Year
 # plot(cma_tristan,type = 'l',col='red',lwd='2',ylim = c(11,17), width=5,height=2, xlab = 'Years', ylab = 'temperature (deg C)')
 # lines(SST$index,cma_gough,col = 'blue',lwd='2')
@@ -30,8 +35,8 @@ p + scale_x_date(limits = c(min, max))
 # #compute simple linear trends
 # tristanlm = lm(cma_tristan~SST$index)
 # goughlm = lm(cma_gough~SST$index)
-# 
-# 
+
+
 # 
 # #add trendlines for both
 # abline(tristanlm,col = 'black',lwd = '3')
